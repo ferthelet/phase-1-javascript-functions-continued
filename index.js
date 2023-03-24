6eaeac75581cd33ce15dd55d2043c494efd4d5bd
Juan Salazar <ferthelet@gmail.com>
2023-03-24 09:59:45 -0500

    adds monday work as fn expression

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function saturdayFun(activity = "roller-skate") {
     // done by 'ear' testing
     return `This Saturday, I want to ${activity}!`;
 }
+
+// function mondayWork(activity = "go to the office") {
+//     return `This Monday, I will ${activity}.`;
+// }
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective() {
+    return 0;
+}
